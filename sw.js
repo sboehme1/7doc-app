5709448e5cf2e2488ddd6ef18264533c2acf81a3
sboehme1 <sboehme1@gmx.de>
2026-05-04 11:39:55 +0800

    fix OneSignal SW conflict

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-﻿/* Service Worker für 7 Days of Change
+﻿/* OneSignal importieren damit beide Service Worker koexistieren */
+try { importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js"); } catch(e) {}
+
+/* Service Worker für 7 Days of Change
    =====================================
    Dieser "unsichtbare Assistent" speichert alle App-Dateien
    auf dem Gerät, damit die App auch offline funktioniert.
@@ -12,7 +15,7 @@
    neu heruntergeladen.
 */
 
-var CACHE_NAME = "7doc-v100";
+var CACHE_NAME = "7doc-v101";
 
 /* Alle Dateien, die offline verfügbar sein sollen */
 var FILES_TO_CACHE = [
